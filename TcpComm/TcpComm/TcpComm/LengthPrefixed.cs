b3c0982bcfb580974a7a075c9c77dbef0ba22909
loser <102282591+sdidbf@users.noreply.github.com>
2026-01-30 13:44:17 +0800

    Add files via upload

---
+++ TcpComm/TcpComm/TcpComm/LengthPrefixed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpCommTest
+{
+    public static class LengthPrefixed
+    {
+        public static void Send(NetworkStream stream, string json)
+        {
+            byte[] body = Encoding.UTF8.GetBytes(json);
+            byte[] len = BitConverter.GetBytes(body.Length);
+
+            //stream.Write(len, 0, 4);
+            stream.Write(body, 0, body.Length);
+        }
+
+        public static string Receive(NetworkStream stream)
+        {
+            //byte[] lenBuf = ReadExact(stream, 4);
+            //int length = BitConverter.ToInt32(lenBuf, 0);
+
+            byte[] body = ReadExact(stream);
+            string str = Encoding.UTF8.GetString(body);
+            return str;
+        }
+
+        private static byte[] ReadExact(NetworkStream stream)
+        {
+            byte[] buf = new byte[1024];
+            int read = 0;
+            while (read <= 0)
+            {
+                int r = stream.Read(buf, read,1024);
+                if (r <= 0) throw new IOException("Socket closed");
+                read += r;
+            }
+            return buf;
+        }
+    }
+}
---
+++ TcpComm/TcpComm/TcpComm/VisionProtocol.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TcpCommTest
+{
+    public class VisionMessage
+    {
+        public string version { get; set; } = "1.0";
+        public string type { get; set; }
+        public int seq { get; set; }
+        public long timestamp { get; set; }
+        public object payload { get; set; }
+    }
+
+    public static class VisionMessageFactory
+    {
+        private static int _seq = 0;
+
+        public static VisionMessage Create(string type, object payload)
+        {
+            return new VisionMessage
+            {
+
+                type = type,
+                seq = System.Threading.Interlocked.Increment(ref _seq),
+                timestamp = DateTimeOffset.UtcNow.Millisecond,
+                payload = payload
+            };
+        }
+    }
+}
